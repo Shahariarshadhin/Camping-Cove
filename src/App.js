908da3259d2023263261f0e1028d2052c4531f3d
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-04-30 01:38:56 +0600

    project initialze

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button, Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CAMPING COVE</h1>
     </div>
   );
 }
